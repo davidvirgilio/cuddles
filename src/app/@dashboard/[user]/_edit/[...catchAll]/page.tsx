a6d9ad48e030a9169c9d3b1a829581b39e4684a5
David Virgilio <dvirgilioc@gmail.com>
2024-08-15 00:46:50 -0600

    Parallel routes default file updated

---
+++ src/app/@dashboard/[user]/_edit/[...catchAll]/page.tsx
@@ -0,0 +1,10 @@
+/*
+* Description: This file defines a React component named CatchAll.
+* This component doesn't render anything, as it simply returns null.
+* Good to know: We use a catch-all route in our @add slot to close the modal because of the behavior described in Active state and navigation. Since client-side navigations to a route that no longer match the slot will remain visible, we need to match the slot to a route that returns null to close the modal.
+* See  https://nextjs.org/docs/app/building-your-application/routing/parallel-routes
+*/
+
+export default function CatchAll() {
+  return null
+}
---
+++ src/app/@dashboard/default.tsx
@@ -0,0 +1,12 @@
+// 'use client'
+// import Page from "./[user]/page"
+// import { useSession } from "next-auth/react"
+
+export default function Default() {
+  // const { data: session } = useSession()
+  // const user = session?.user.id
+  // const params = {user}
+
+  // return <Page params={params} />
+  return null
+}
