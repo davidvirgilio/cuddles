f2d7d4feaeff3cb589e79535fc9af45709072c57
David Virgilio <dvirgilioc@gmail.com>
2024-09-09 01:54:23 -0600

    Infinite scrolling implemented

--- src/ui/components/show-posts/show-posts.tsx
+++ src/ui/components/show-posts/show-posts.tsx
@@ -37,7 +37,7 @@ export default function Posts({ initialPosts, isProfile = false}: PostsProps ){
         if (inView) {
           loadMorePosts()
         }
-      }, [inView])
+      }, [inView, isProfile, offset, posts, userId])
 
     return(
         <div>
